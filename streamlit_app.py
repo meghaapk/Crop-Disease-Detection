8d7464b5d2371fbc0f070a77ef4095e0951da09a
PradHolla <pradholla@gmail.com>
2022-03-26 20:40:16 +0530

    Code Refactorings

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,24 +6,33 @@ from tempfile import NamedTemporaryFile
 st.set_page_config(page_title="Crop Disease Identification", layout="wide", page_icon="🌱")
 st.header("Crop Disease Identification")
 st.write("Created by [Megha](https://github.com/meghaapk).")
-st.subheader("This app predicts the disease of the crop")
+st.subheader("This app predicts the disease of the crop. Add a proper description here.")
 class_names = ["Early Blight", "Late Blight", "Healthy"]
 
 # @st.cache()
-# def load_model():
-#     model = tf.keras.models.load_model('potatoes.h5')
-#     return model
+def load_model():
+    if crop == "Coffee":
+        return tf.keras.models.load_model('models/coffee.h5')
+    elif crop == "Pepper":
+        return tf.keras.models.load_model('models/pepper.h5')
+    elif crop == "Potato":
+        return tf.keras.models.load_model('models/potatoes.h5')
+    else:
+        return tf.keras.models.load_model('models/tomatoes.h5')
 
 def predict(image):
     my_img = tf.keras.utils.load_img(image, target_size=(256, 256))
     img_array = tf.keras.utils.img_to_array(my_img)
     img_array = tf.expand_dims(img_array, 0)
-    model = tf.keras.models.load_model('potatoes.h5')
+    model = load_model()
     pred = model.predict(img_array)
     pred = np.argmax(pred[0])
     return pred
 
 if __name__ == "__main__":
+    st.sidebar.title("Give an appropriate sidebar heading")
+    crop = st.sidebar.radio("Select the crop", ("Coffee", "Pepper", "Potato", "Tomato"))
+    st.write("##### You selected: {}".format(crop))
     option = st.sidebar.selectbox("Select an option", ["Upload Image", "Capture Image"])
     if option == "Upload Image":
         image = st.sidebar.file_uploader("Upload an image", type=["jpg", "png"])
