927abae17cc869cb72a7c30cd1a15dae810a0bb9
meghaapk <76991530+meghaapk@users.noreply.github.com>
2022-04-27 16:20:18 +0530

    fixed issues

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,32 +2,45 @@ import streamlit as st
 import tensorflow as tf
 import numpy as np
 from tempfile import NamedTemporaryFile
+from PIL import Image   
 
 st.set_page_config(page_title="Crop Disease Identification", layout="wide", page_icon="🌱")
 st.header("Crop Disease Identification")
-st.write("Created by [Maggy](https://github.com/meghaapk).")
-st.subheader("This app predicts the disease of the crop. Add a proper description here.")
-class_names = ["Early Blight", "Late Blight", "Healthy"]
+# st.markdown("<h1 style='text-align: center'>Crop Disease Identification</h1>", unsafe_allow_html=True)
+image=Image.open('Images/logo.png')
+col1, col2, col3 = st.columns([3, 3, 1])
+col2.image(image, width=175)
+# st.image(image)
+st.write("Created by [Kiran](https://github.com/kirandr04) [Megha](https://github.com/meghaapk) [Nidhisha](https://github.com/NidhishaRao).")
+st.markdown("""
+<style>
+.big-font {
+    font-size:22px !important;
+}
+</style>
+""", unsafe_allow_html=True)
+st.markdown('<p class="big-font">Ever grew a plant and wanted to know if they were growing healthy?</p>', unsafe_allow_html=True)
+st.markdown('<p class="big-font">You can now identify if your plant has any disease just by scanning your plant leaves.This app predicts the disease of the crop.</p>', unsafe_allow_html=True)
 
 # @st.cache()
 def load_model():
     if crop == "Coffee":
-        return tf.keras.models.load_model('models/coffee.h5')
+        return tf.keras.models.load_model('models/coffee.h5'),["Coffee Miner Disease","Coffee Rust Disease"]
     elif crop == "Pepper":
-        return tf.keras.models.load_model('models/pepper.h5')
+        return tf.keras.models.load_model('models/pepper.h5'),["Pepper bell Bacterial spot Disease", "Pepperbell healthy"]
     elif crop == "Potato":
-        return tf.keras.models.load_model('models/potatoes.h5')
+        return tf.keras.models.load_model('models/potatoes.h5'),["Early Blight Disease", "Late Blight Disease", "Healthy"]
     else:
-        return tf.keras.models.load_model('models/tomatoes.h5')
+        return tf.keras.models.load_model('models/tomatoes.h5'),['Tomato Early Blight Disease', 'Tomato Late Blight Disease', 'Tomato_healthy']
 
 def predict(image):
     my_img = tf.keras.utils.load_img(image, target_size=(256, 256))
     img_array = tf.keras.utils.img_to_array(my_img)
     img_array = tf.expand_dims(img_array, 0)
-    model = load_model()
+    model,class_names = load_model()
     pred = model.predict(img_array)
     pred = np.argmax(pred[0])
-    return pred
+    return pred,class_names
 
 if __name__ == "__main__":
     st.sidebar.title("Give an appropriate sidebar heading")
@@ -45,8 +58,8 @@ if __name__ == "__main__":
             st.image(image)
             with st.spinner("Analyzing..."):
                 temp_file.write(image.getvalue())
-                pred = predict(temp_file.name)
+                pred,class_names = predict(temp_file.name)
                 st.balloons()
-                st.success("##### The predicted disease is {}".format(class_names[pred]))
+                st.success("##### The prediction is {}".format(class_names[pred]))
         else:
             st.error("Please upload an image")
